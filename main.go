43cb2e6f0e985eb6dcd5a17962274150963f62f9
Jacob Foster <dalindeck@gmail.com>
2026-07-01 19:36:43 -0700

    feat: added metrics middleware

--- main.go
+++ main.go
@@ -1,6 +1,14 @@
 package main
 
-import "net/http"
+import (
+	"fmt"
+	"net/http"
+	"sync/atomic"
+)
+
+type apiConfig struct {
+	fileserverHits atomic.Int32
+}
 
 func main() {
 	serveMux := http.NewServeMux()
@@ -9,12 +17,37 @@ func main() {
 		Handler: serveMux,
 	}
 
-	serveMux.Handle("/app/", http.StripPrefix("/app", http.FileServer(http.Dir("."))))
+	cfg := apiConfig{}
+
+	serveMux.Handle("/app/", http.StripPrefix("/app", cfg.middlewareMetricsInc(http.FileServer(http.Dir(".")))))
 	serveMux.HandleFunc("/healthz", func(res http.ResponseWriter, req *http.Request) {
 		res.Header().Add("Content-Type", "text/plain; charset=utf-8")
 		res.WriteHeader(200)
 		res.Write([]byte("OK"))
 	})
+	serveMux.HandleFunc("/metrics", cfg.metricsHandler)
+	serveMux.HandleFunc("/reset", cfg.resetHandler)
 
 	server.ListenAndServe()
+}
+
+func (cfg *apiConfig) metricsHandler(res http.ResponseWriter, _ *http.Request) {
+	res.Header().Add("Content-Type", "text/plain; charset=utf-8")
+	res.WriteHeader(200)
+	res.Write([]byte(fmt.Sprintf("Hits: %d", cfg.fileserverHits.Load())))
+}
+
+func (cfg *apiConfig) resetHandler(res http.ResponseWriter, _ *http.Request) {
+	cfg.fileserverHits.Store(0)
+	res.Header().Add("Content-Type", "text/plain; charset=utf-8")
+	res.WriteHeader(200)
+	res.Write([]byte(fmt.Sprintf("Reset to: %d", cfg.fileserverHits.Load())))
+}
+
+func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(res http.ResponseWriter, req *http.Request) {
+		cfg.fileserverHits.Add(1)
+
+		next.ServeHTTP(res, req)
+	})
 }
